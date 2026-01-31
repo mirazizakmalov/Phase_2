3fa3bd75add3867a3eb7da63550926520ae393c8
Miraziz <mirazizakmalov@gmail.com>
2026-01-30 19:56:51 -0500

    Starting project

---
+++ assembly.py
@@ -0,0 +1,98 @@
+import sys
+import os
+
+R_TYPE = {
+    "add":  {"opcode": 0x33, "funct3": 0x0, "funct7": 0x00},
+    "sub":  {"opcode": 0x33, "funct3": 0x0, "funct7": 0x20},
+    "sll":  {"opcode": 0x33, "funct3": 0x1, "funct7": 0x00},
+    "slt":  {"opcode": 0x33, "funct3": 0x2, "funct7": 0x00},
+    "sltu": {"opcode": 0x33, "funct3": 0x3, "funct7": 0x00},
+    "xor":  {"opcode": 0x33, "funct3": 0x4, "funct7": 0x00},
+    "srl":  {"opcode": 0x33, "funct3": 0x5, "funct7": 0x00},
+    "sra":  {"opcode": 0x33, "funct3": 0x5, "funct7": 0x20},
+    "or":   {"opcode": 0x33, "funct3": 0x6, "funct7": 0x00},
+    "and":  {"opcode": 0x33, "funct3": 0x7, "funct7": 0x00},
+}
+
+
+I_TYPE_ALU = {
+    "addi":  {"opcode": 0x13, "funct3": 0x0},
+    "slti":  {"opcode": 0x13, "funct3": 0x2},
+    "sltiu": {"opcode": 0x13, "funct3": 0x3},
+    "xori":  {"opcode": 0x13, "funct3": 0x4},
+    "ori":   {"opcode": 0x13, "funct3": 0x6},
+    "andi":  {"opcode": 0x13, "funct3": 0x7},
+}
+
+
+I_TYPE_SHIFT = {
+    "slli": {"opcode": 0x13, "funct3": 0x1, "funct7": 0x00},
+    "srli": {"opcode": 0x13, "funct3": 0x5, "funct7": 0x00},
+    "srai": {"opcode": 0x13, "funct3": 0x5, "funct7": 0x20},
+}
+
+
+I_TYPE_LOAD = {
+    "lb":  {"opcode": 0x03, "funct3": 0x0},
+    "lh":  {"opcode": 0x03, "funct3": 0x1},
+    "lw":  {"opcode": 0x03, "funct3": 0x2},
+    "lbu": {"opcode": 0x03, "funct3": 0x4},
+    "lhu": {"opcode": 0x03, "funct3": 0x5},
+}
+
+
+S_TYPE_STORE = {
+    "sb": {"opcode": 0x23, "funct3": 0x0},
+    "sh": {"opcode": 0x23, "funct3": 0x1},
+    "sw": {"opcode": 0x23, "funct3": 0x2},
+}
+
+
+B_TYPE_BRANCH = {
+    "beq":  {"opcode": 0x63, "funct3": 0x0},
+    "bne":  {"opcode": 0x63, "funct3": 0x1},
+    "blt":  {"opcode": 0x63, "funct3": 0x4},
+    "bge":  {"opcode": 0x63, "funct3": 0x5},
+    "bltu": {"opcode": 0x63, "funct3": 0x6},
+    "bgeu": {"opcode": 0x63, "funct3": 0x7},
+}
+
+U_TYPE = {
+    "lui":   {"opcode": 0x37},
+    "auipc": {"opcode": 0x17},
+}
+
+
+J_TYPE = {
+    "jal": {"opcode": 0x6F},
+}
+
+I_TYPE_JALR = {
+    "jalr": {"opcode": 0x67, "funct3": 0x0},
+}
+
+I_TYPE_SYSTEM = {
+    "ecall":  {"opcode": 0x73, "funct3": 0x0, "imm12": 0x000},
+    "ebreak": {"opcode": 0x73, "funct3": 0x0, "imm12": 0x001},
+
+def intialScan():
+    file = open()
+
+
+def main():
+    basePath = sys.argv[1]
+    asmFile = sys.argv[2]
+    asmPath = os.path.join(basePath, asmFile)
+    
+    //Read assembly file
+    with open(asmPath, 'r') as f:
+        lines = f.readlines()
+
+            
+            
+            
+
+if __name__ == "__main__":
+    main()
+        
+
